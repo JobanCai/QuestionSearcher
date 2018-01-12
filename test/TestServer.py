3906451f558ef59cb710590d54566d227f10a46c
lizhengda <lizhengda@starfire.site>
2018-01-13 01:22:28 +0800

    修改

--- test/TestServer.py
+++ test/TestServer.py
@@ -1,5 +1,6 @@
 import web
 import codecs
+import random
 
 urls = (
     '/(.*)', 'hello'
@@ -10,23 +11,30 @@ app = web.application(urls, globals())
 
 class hello:
     json_list = {
-        '1': '1515761747.json',
-        '2': '1515761761.json',
-        '3': '1515762118.json',
-        '4': '1515762154.json',
-        '5': '1515762168.json',
-        '6': '1515762210.json'
+        1: '1515762118.json',
+        2: '1515762154.json',
+        3: '1515762168.json',
+        4: '1515762221.json',
+        5: '1515762269.json',
+        6: '1515762320.json',
+        8: '1515762395.json',
+        9: '1515762406.json',
+        10: '1515762432.json',
+        11: '1515762537.json',
+        12: '1515762559.json',
+        13: '1515762614.json',
+        14: '1515762673.json',
+        15: '1515762765.json',
+        7: '1515762471.json'
     }
 
     def GET(self, name):
         if not name:
             json = 'not JSON!'
         else:
-            f = codecs.open("../test_data/json/%s" % self.json_list[name], "r", "utf-8")
+            f = codecs.open("../test_data/json/%s" % self.json_list[random.randint(1, 16)], "r", "utf-8")
             json = f.read()
             f.close()
-        print(json)
-
         return json
 
 
