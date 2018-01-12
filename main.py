54c7a8ec4c2c79b0f35d3e6675d5b9bdcf703874
lizhengda <lizhengda@starfire.site>
2018-01-12 22:55:54 +0800

    change main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import problem, web, algo
+import problem, web_util, algo
 
 def main():
   problem,  = problem.get_chongding_by_api()
-  web.open_browser(web.BAIDU_BASE + '?wd=' + problem)
+  web_util.open_browser(web_util.BAIDU_BASE + '?wd=' + problem)
   tags = algo.extract_tags(problem)
-  context = web.request_keywords(tags, 'baidu')
-  respones = web.segment_html(context)
+  context = web_util.request_keywords(tags, 'baidu')
+  respones = web_util.segment_html(context)
   
 
 if __name__ == '__main__':
